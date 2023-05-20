d00be163ad73c8ea718bf9510f287cee3891e288
AthulKkumar <athulkkumar5@gmail.com>
2023-05-20 18:59:04 +0530

    Inital stage of the task

---
+++ src/components/ProductPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ProductPage = () => {
+    return (
+        <div className='container mt-4'>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ProductPage
